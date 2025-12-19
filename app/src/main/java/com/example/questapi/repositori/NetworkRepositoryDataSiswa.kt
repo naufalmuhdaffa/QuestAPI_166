33bd03dedaebebbffbbfe9e16183ff81fb5bb7cf
naufalmuhdaffa <naufaldaffa878@gmail.com>
2025-12-19 08:22:35 +0700

    Add class

---
+++ app/src/main/java/com/example/questapi/repositori/NetworkRepositoryDataSiswa.kt
@@ -0,0 +1,11 @@
+package com.example.questapi.repositori
+
+import com.example.questapi.api.service.ServiceApiSiswa
+import com.example.questapi.model.data.DataSiswa
+
+class NetworkRepositoryDataSiswa(
+    private val serviceApiSiswa: ServiceApiSiswa
+):RepositoryDataSiswa{
+    override suspend fun getDataSiswa() : List<DataSiswa> = serviceApiSiswa.getSiswa()
+    override suspend fun postDataSiswa(dataSiswa : DataSiswa):retrofit2.Response<Void> = serviceApiSiswa.postSiswa(dataSiswa)
+}
