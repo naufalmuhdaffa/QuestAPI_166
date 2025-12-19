fda5044c9fbfebea7fe9ddbb2698652a3cb4f120
naufalmuhdaffa <naufaldaffa878@gmail.com>
2025-12-19 08:28:36 +0700

    Add interface

---
+++ app/src/main/java/com/example/questapi/repositori/container/ContainerApp.kt
@@ -0,0 +1,7 @@
+package com.example.questapi.repositori.container
+
+import com.example.questapi.repositori.RepositoryDataSiswa
+
+interface ContainerApp {
+    val repositoryDataSiswa : RepositoryDataSiswa
+}
